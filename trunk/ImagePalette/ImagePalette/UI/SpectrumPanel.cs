8f2197c188d39601670a9455706c20c2983f4df9
joaonc@gmail.com <joaonc@gmail.com@f75c8837-2b6f-4ddd-e092-2965843271a6>
2014-03-10 22:36:40 +0000

    Added ColorUtil and moved common functionality regarding Color there.

--- trunk/ImagePalette/ImagePalette/UI/SpectrumPanel.cs
+++ trunk/ImagePalette/ImagePalette/UI/SpectrumPanel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -7,6 +8,10 @@ namespace ImagePalette.UI
 {
     public partial class SpectrumPanel : UserControl
     {
+        private DataGridView dgvIndexed;
+        private ImagePaletteParameters parameters;
+        Graphics graphics;
+
         public SpectrumPanel()
         {
             InitializeComponent();
@@ -22,7 +27,21 @@ namespace ImagePalette.UI
             PaintPanel();
         }
 
+        public void SetData(DataGridView dgvIndexed, ImagePaletteParameters parameters)
+        {
+            this.dgvIndexed = dgvIndexed;
+            this.parameters = parameters;
+
+            PaintPanel();
+        }
+
         private void PaintPanel()
+        {
+            PaintSpectrum();
+            PaintIndexed();
+        }
+
+        private void PaintSpectrum()
         {
             // Create the color spectrum.
             // From http://stackoverflow.com/questions/2288498/how-do-i-get-a-rainbow-color-gradient-in-c
@@ -32,17 +51,28 @@ namespace ImagePalette.UI
             double posIncrease = (double)panel.Width / (double)numColors;
             Size size = new Size(Convert.ToInt16(Math.Ceiling(posIncrease)), panel.Height);
             Point location = new Point(0, 0);
-            Graphics graphics = panel.CreateGraphics();
+            graphics = panel.CreateGraphics();
 
             for (double i = 0, x = 0; i < 1; i += colorIncrease, x += posIncrease)
             {
-                Color color = ImagePaletteProcess.HSL2RGB(i, 0.5, 0.5);
+                Color color = ColorUtil.HSL2RGB(i, 0.5, 0.5);
                 SolidBrush brush = new SolidBrush(color);
 
                 location.X = (int)x;
                 Rectangle rect = new Rectangle(location, size);
                 
                 graphics.FillRectangle(brush, rect);
+            }
+        }
+
+        private void PaintIndexed()
+        {
+            if (dgvIndexed != null)
+            {
+                foreach (DataRowView row in dgvIndexed.Rows)
+                {
+
+                }
             }
         }
     }
---
+++ trunk/ImagePalette/ImagePalette/Util/ColorUtil.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Drawing;
+
+namespace ImagePalette
+{
+    public static class ColorUtil
+    {
+        /// <summary>
+        /// Given H,S,L in range of 0-1
+        /// Returns a Color (RGB struct) in range of 0-255
+        /// 
+        /// Taken from http://www.geekymonkey.com/Programming/CSharp/RGB2HSL_HSL2RGB.htm
+        /// </summary>
+        /// <param name="h"></param>
+        /// <param name="sl"></param>
+        /// <param name="l"></param>
+        /// <returns></returns>
+        public static Color HSL2RGB(double h, double sl, double l)
+        {
+            double v;
+            double r, g, b;
+
+            r = l;   // default to gray
+            g = l;
+            b = l;
+            v = (l <= 0.5) ? (l * (1.0 + sl)) : (l + sl - l * sl);
+            if (v > 0)
+            {
+                double m;
+                double sv;
+                int sextant;
+                double fract, vsf, mid1, mid2;
+
+                m = l + l - v;
+                sv = (v - m) / v;
+                h *= 6.0;
+                sextant = (int)h;
+                fract = h - sextant;
+                vsf = v * sv * fract;
+                mid1 = m + vsf;
+                mid2 = v - vsf;
+                switch (sextant)
+                {
+                    case 0:
+                        r = v;
+                        g = mid1;
+                        b = m;
+                        break;
+                    case 1:
+                        r = mid2;
+                        g = v;
+                        b = m;
+                        break;
+                    case 2:
+                        r = m;
+                        g = v;
+                        b = mid1;
+                        break;
+                    case 3:
+                        r = m;
+                        g = mid2;
+                        b = v;
+                        break;
+                    case 4:
+                        r = mid1;
+                        g = m;
+                        b = v;
+                        break;
+                    case 5:
+                        r = v;
+                        g = m;
+                        b = mid2;
+                        break;
+                }
+            }
+
+            Color rgb = Color.FromArgb(255, Convert.ToByte(r * 255.0f), Convert.ToByte(g * 255.0f), Convert.ToByte(b * 255.0f));
+            return rgb;
+        }
+
+        /// <summary>
+        /// Given a Color in range of 0-255
+        /// Return H,S,L in range of 0-1
+        /// 
+        /// Taken from http://www.geekymonkey.com/Programming/CSharp/RGB2HSL_HSL2RGB.htm
+        /// </summary>
+        /// <param name="rgb"></param>
+        /// <param name="h"></param>
+        /// <param name="s"></param>
+        /// <param name="l"></param>
+        public static void RGB2HSL(Color rgb, out double h, out double s, out double l)
+        {
+            double r = rgb.R / 255d;
+            double g = rgb.G / 255d;
+            double b = rgb.B / 255d;
+            double v;
+            double m;
+            double vm;
+            double r2, g2, b2;
+
+            h = 0; // default to black
+            s = 0;
+            l = 0;
+            v = Math.Max(r, g);
+            v = Math.Max(v, b);
+            m = Math.Min(r, g);
+            m = Math.Min(m, b);
+            l = (m + v) / 2.0;
+            if (l <= 0.0)
+            {
+                return;
+            }
+            vm = v - m;
+            s = vm;
+            if (s > 0.0)
+            {
+                s /= (l <= 0.5) ? (v + m) : (2.0 - v - m);
+            }
+            else
+            {
+                return;
+            }
+            r2 = (v - r) / vm;
+            g2 = (v - g) / vm;
+            b2 = (v - b) / vm;
+            if (r == v)
+            {
+                h = (g == m ? 5.0 + b2 : 1.0 - g2);
+            }
+            else if (g == v)
+            {
+                h = (b == m ? 1.0 + r2 : 3.0 - b2);
+            }
+            else
+            {
+                h = (r == m ? 3.0 + g2 : 5.0 - r2);
+            }
+            h /= 6.0;
+        }
+
+        /// <summary>
+        /// Calculates the distance between two colors, adjusted for the human eye.
+        /// More details on the theory here: http://www.compuphase.com/cmetric.htm
+        /// Important note from the link above:
+        /// This formula has results that are very close to L*u*v* (with the modified lightness curve) and, more importantly,
+        /// it is a more stable algorithm: it does not have a range of colours where it suddenly gives far from optimal results.
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static double ColorDistance(Color c1, Color c2)
+        {
+            long rmean = ((long)c1.R + (long)c2.R) / 2;
+            long r = (long)c1.R - (long)c2.R;
+            long g = (long)c1.G - (long)c2.G;
+            long b = (long)c1.B - (long)c2.B;
+
+            return Math.Sqrt((((512 + rmean) * r * r) >> 8) + 4 * g * g + (((767 - rmean) * b * b) >> 8));
+        }
+    }
+}
